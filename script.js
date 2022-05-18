d2b12eac6c0c4e733c408118293b301e13354e42
avagyanmher <78161847+avagyanmher@users.noreply.github.com>
2022-05-18 17:17:09 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.title', {delay: 700});
+ScrollReveal().reveal('.second__title', { delay: 1000 });
+ScrollReveal().reveal('.third__title', { delay: 1300, });
+ScrollReveal().reveal('.button__wrapper', { delay: 1600, });
+ScrollReveal().reveal('.header', {delay: 00});
+ScrollReveal().reveal('.hero__main', {delay: 500});
